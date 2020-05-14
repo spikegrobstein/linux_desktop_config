b80b3824a4755fd9d7b5381b7e76b307ef2af5da
Spike Grobstein <me@spike.cx>
2020-05-14 16:39:36 -0700

    cleanup + lock screen image

--- dotfiles/config/i3/lock.sh
+++ dotfiles/config/i3/lock.sh
@@ -2,6 +2,11 @@
 
 set -e
 xset s off dpms 0 10 0
-i3lock --color=4c7899 --ignore-empty-password --show-failed-attempts --nofork
+
+i3lock \
+  --color=4c7899 \
+  --ignore-empty-password \
+  --show-failed-attempts \
+  --nofork
 xset s off -dpms
 
